e8082052ef1995fc6508baab470110c2c098785c
Дмитрий Швецов <shvetsov_d@tochka.com>
2022-06-25 18:01:32 +0500

    hide secrets

--- web.py
+++ web.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text, insert
+from sqlalchemy import text
+import os
+
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://db_user:db_password@db/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@db/{DB_NAME}'
 
 db = SQLAlchemy(app)
 
@@ -13,7 +19,7 @@ def index():
     db.session.execute(
         text(
             '''
-            CREATE TABLE test (
+            CREATE TABLE IF NOT EXISTS test (
                 id SERIAL PRIMARY KEY,
                 field VARCHAR(255)
             )
